acfaf1d438772ece03227735528bb5d2c591ee4f
Vlad-Winner <vlad-win@ya.ru>
2022-10-27 19:59:08 +0700

    добавил проверку вводимых ответов "д/н" А так-же немного оптимизировал код

--- password_generator.py
+++ password_generator.py
@@ -7,10 +7,19 @@ PUNCTUATION = '!#$%&*+-=?@^_'
 
 chars = ''
 
+#функции проверок вводимых пользователем данных
 def is_valid_num(text):
     if text.isdigit() and int(text) > 0:
         return True
+    print('Необходимо ввести положительное число, попробуйте еще раз: ', end='')
     return False
+
+def is_bool(text):
+    if text.lower() == 'д' or text.lower() == 'н':
+        return True
+    else:
+        print('Необходимо ввести "д" или "н", попробуйте ещё раз: ', end='')
+        return False
 
 #Ввод данных от пользователя
 clarification = '("д" = да, "н" = нет): '
@@ -18,30 +27,48 @@ clarification = '("д" = да, "н" = нет): '
 print('Сколько паролей необходимо сгенерировать?: ', end='')
 count = input()
 while not is_valid_num(count):
-    count = input('Необходимо ввести положительное число, попробуйте еще раз: ')
+    count = input()
 
 print('Какая длина пароля необходима?: ', end='')
 length = input()
 while not is_valid_num(length):
-    length = input('Необходимо ввести положительное число, попробуйте еще раз: ')
+    length = input()
 
+print(f'Включать цифры в пароль? {clarification}', end='')
+num = input()
+while not is_bool(num):
+    num = input()
 
-num = input(f'Включать цифры в пароль? {clarification}')
-upp = input(f'Включать заглавные буквы? {clarification}')
-low = input(f'Включать буквы в нижнем регистре? {clarification}')
-pun = input(f'Включать пунктуацию? {clarification}')
-exceptions = input(f'Исключать неоднозначные символы(il1Lo0O)? {clarification}')
+print(f'Включать заглавные буквы? {clarification}', end='')
+upp = input()
+while not is_bool(upp):
+    upp = input()
+
+print(f'Включать буквы в нижнем регистре? {clarification}', end='')
+low = input()
+while not is_bool(low):
+    low = input()
+
+print(f'Включать пунктуацию? {clarification}', end='')
+pun = input()
+while not is_bool(pun):
+    pun = input()
+
+print(f'Исключать неоднозначные символы(il1Lo0O)? {clarification}', end='')
+exceptions = input()
+while not is_bool(exceptions):
+    exceptions = input()
 
 #формируем строку допустимых символов
-if num == 'д':
+if num.lower() == 'д':
     chars += DIGITS
-if upp == 'д':
+if upp.lower() == 'д':
     chars += LOWERCASE_LETTERS
-if low == 'д':
+if low.lower() == 'д':
     chars += UPPERCASE_LETTERS
-if pun == 'д':
+if pun.lower() == 'д':
     chars += PUNCTUATION
-if exceptions == 'д':
+if exceptions.lower() == 'д':
     chars = chars.replace('i', '')
     chars = chars.replace('l', '')
     chars = chars.replace('1', '')
