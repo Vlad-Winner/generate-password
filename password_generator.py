0b9b35aa9e477bbe996741684cef628e3b24af6f
Vlad-Winner <vlad-win@ya.ru>
2022-10-27 19:46:16 +0700

    добавил проверку вводимых чисел

--- password_generator.py
+++ password_generator.py
@@ -7,10 +7,25 @@ PUNCTUATION = '!#$%&*+-=?@^_'
 
 chars = ''
 
+def is_valid_num(text):
+    if text.isdigit() and int(text) > 0:
+        return True
+    return False
+
 #Ввод данных от пользователя
 clarification = '("д" = да, "н" = нет): '
-count = int(input('Сколько паролей необходимо сгенерировать?: '))
-length = int(input('Какая длина пароля необходима?: '))
+
+print('Сколько паролей необходимо сгенерировать?: ', end='')
+count = input()
+while not is_valid_num(count):
+    count = input('Необходимо ввести положительное число, попробуйте еще раз: ')
+
+print('Какая длина пароля необходима?: ', end='')
+length = input()
+while not is_valid_num(length):
+    length = input('Необходимо ввести положительное число, попробуйте еще раз: ')
+
+
 num = input(f'Включать цифры в пароль? {clarification}')
 upp = input(f'Включать заглавные буквы? {clarification}')
 low = input(f'Включать буквы в нижнем регистре? {clarification}')
@@ -37,12 +52,12 @@ if exceptions == 'д':
 
 #функция создания пароля
 def generate_password(length, chars):
-    if len(chars) >= length:
-        print(''.join(random.sample(chars, length)))
+    if len(chars) >= int(length):
+        print(''.join(random.sample(chars, int(length))))
     elif len(chars) != 0:
-        print(''.join(random.sample(chars, len(chars)) + [random.choice(chars) for _ in range(length - len(chars))]))
+        print(''.join(random.sample(chars, len(chars)) + [random.choice(chars) for _ in range(int(length) - len(chars))]))
     else:
         print('Нет символов для генерации вашего пароля!')
 
-for _ in range(count):
+for _ in range(int(count)):
     generate_password(length, chars)
